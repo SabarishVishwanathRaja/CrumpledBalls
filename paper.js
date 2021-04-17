38ed0e5bebc10c46ae159cdfa374c9f3b26cc5a7
SabarishVishwanathRaja <74968443+SabarishVishwanathRaja@users.noreply.github.com>
2021-04-17 09:14:08 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper {
+    constructor(x,y,r) {
+        var options={
+            restitution:0.3,
+            density:1.5
+        }
+        this.image = loadImage("paper.png")
+        this.body = Bodies . circle(x,y,r,options);
+        this.r = r;
+        World.add(world,this.body)
+
+    }
+    display() {
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.r, this.r);
+        pop();
+
+    }
+}
